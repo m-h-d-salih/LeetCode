a94f41efe1fdf79516f7abf369ccb8499de34a3d
Mohammed Salih k <salihsha656@gmail.com>
2024-12-27 10:19:47 +0530

    Time: 11 ms (7.43%), Space: 54 MB (5.06%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    const even=nums.filter(i=>i%2===0).sort((a,b)=>a-b);
+    const odd=nums.filter(i=>i%2!==0).sort((a,b)=>a-b);
+    return [...even,...odd]
+};
