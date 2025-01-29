8a24ec1d22972022fe0469c4f5ccc8b97ef63ff2
Mohammed Salih k <salihsha656@gmail.com>
2025-01-29 14:14:49 +0530

    Time: 1 ms (92.99%), Space: 52.7 MB (77.07%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {string[]} commands
+ * @return {number}
+ */
+var finalPositionOfSnake = function(n, commands) {
+    let result=0;
+    for(let i=0;i<commands.length;i++){
+        if(commands[i]==='RIGHT'){
+            result+=1;
+        }else if(commands[i]==="DOWN"){
+            result+=n;
+        }else if(commands[i]==="UP"){
+            result-=n;
+        }else if(commands[i]==="LEFT"){
+            result-=1;
+        }
+    }
+    return result;
+};
