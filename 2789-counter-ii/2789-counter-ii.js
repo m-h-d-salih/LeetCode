e40176804ee1bed30288c93291376a0a9c3c2e7f
Mohammed Salih k <salihsha656@gmail.com>
2025-03-03 14:51:23 +0530

    Time: 28 ms (99.97%), Space: 57.6 MB (6.74%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  let n=init;
+  return {
+      increment:()=>++init,
+      decrement:()=>--init,
+      reset:()=>init=n,
+  }
+};
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
