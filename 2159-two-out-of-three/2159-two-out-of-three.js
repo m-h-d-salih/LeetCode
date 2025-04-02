f918625827489196b1db509047ba18c9ac772fd7
Mohammed Salih k <salihsha656@gmail.com>
2025-04-02 11:06:10 +0530

    Time: 7 ms (40.33%), Space: 56.2 MB (74.93%) - LeetHub

---
+++ 2159-two-out-of-three/2159-two-out-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @return {number[]}
+ */
+var twoOutOfThree = function(nums1, nums2, nums3) {
+    const arr2=[...Array.from(new Set(nums1))]
+    const arr=[...Array.from(new Set(nums1)),...Array.from(new Set(nums2)),...Array.from(new Set(nums3))];
+    return Array.from(new Set(arr.filter(item=>arr.indexOf(item)!==arr.lastIndexOf(item))) );
+};
