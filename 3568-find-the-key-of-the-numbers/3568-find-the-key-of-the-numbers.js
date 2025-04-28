296fdbdd9db13637b6f357b5be8b66146d6c3865
Mohammed Salih k <salihsha656@gmail.com>
2025-04-28 12:30:15 +0530

    Time: 1 ms (77.78%), Space: 56.5 MB (29.63%) - LeetHub

---
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @param {number} num3
+ * @return {number}
+ */
+var generateKey = function (num1, num2, num3) {
+    num1 = num1.toString().padStart(4, '0');
+    num2 = num2.toString().padStart(4, '0');
+    num3 = num3.toString().padStart(4, '0');
+    let res = "";
+    for (let i = 0; i < 4; i++) {
+        res += Math.min(num1[i], num2[i], num3[i]);
+    }
+    return parseInt(res, 10);
+};
