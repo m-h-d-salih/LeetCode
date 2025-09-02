418052360fcded1ed280c3d7724c5b73e39d4eb9
Mohammed Salih k <salihsha656@gmail.com>
2025-09-02 11:37:31 +0530

    Time: 0 ms (100%), Space: 17.2 MB (100%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count=0
+        balance=0
+        for i in s:
+            if i == "R":
+                count+=1
+            else :
+                count-=1
+            if count==0:
+                balance+=1
+
+        return balance
+            
+        
